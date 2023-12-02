cbeae909f9a3a0575700b3982ec0a7ae3bb167bc
Jose Maia <maianeto2014@gmail.com>
2023-12-02 10:07:15 -0300

    Method addDirectory implemented

--- src/br/ufrn/imd/service/FileManagementService.java
+++ src/br/ufrn/imd/service/FileManagementService.java
@@ -3,6 +3,7 @@ package br.ufrn.imd.service;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -26,76 +27,14 @@ public class FileManagementService {
 	}
 
 	/**
-     * Metodo que comanda a inicializacao das playlists.
-     * @param user o usuario cujas playlists serao carregadas.
-     * @return Uma ArrayList com as playlists carregadas.
+     * Method that manages the playlists initialization.
+     * @param user the user whose playlists will be loaded.
+     * @return An ArrayList with the loaded playlists.
      */
 	public ArrayList<Playlist> loadPlaylists(UserVip user) { 
 		ArrayList<Playlist> playlists = new ArrayList<Playlist>(); 
 		
 		int userId = user.getId();
-		
-		BufferedReader buffRead;
-		int playlistId = 1;
-		
-		try {
-			while(true) {
-				buffRead = new BufferedReader(new FileReader(getClass().getResource("/resources/data/playlists/playlist" + playlistId + ".txt").getFile()));
-				
-				String line = buffRead.readLine();
-				
-				if(line == null) {
-					break;
-				}
-				
-				int fileUserId = Integer.parseInt(line);
-				
-				if(fileUserId != userId) {
-					playlistId++;
-					continue;
-				}
-				
-				line = buffRead.readLine();
-				
-				String userName = line;
-				line = buffRead.readLine();
-				
-				line = buffRead.readLine();
-				
-				String playlistName = line;
-				line = buffRead.readLine();
-				
-				Playlist p = new Playlist(playlistId);
-				p.setName(playlistName);
-				
-				while(true) {
-					if(line != null) {
-						String songPath = line;
-						Music m = new Music();
-						m.setPath(songPath);
-						
-						p.addSong(m);
-					}
-					else {
-						break;
-					}
-				}
-				
-				buffRead.close();
-				
-			}
-		} catch (IOException e) {
-			//e.printStackTrace();
-		}
-		
-		return playlists; 
-	}
-	
-	
-	public ArrayList<Playlist> loadPlaylists(int idUsuario) { 
-		ArrayList<Playlist> playlists = new ArrayList<Playlist>(); 
-		
-		int userId = idUsuario;
 		
 		BufferedReader buffRead;
 		int playlistId = 0;
@@ -104,9 +43,7 @@ public class FileManagementService {
 			while(true) {
 				playlistId++;
 				
-				
 				File directory = new File("/home/maia/eclipse-workspace/MediaPlayer/src/resources/data/playlists");
-				
 				int fileCount = directory.list().length;
 				
 				
@@ -115,8 +52,6 @@ public class FileManagementService {
 					
 				
 				buffRead = new BufferedReader(new FileReader(getClass().getResource("/resources/data/playlists/playlist" + playlistId + ".txt").getFile()));
-				
-				
 				
 				String line = buffRead.readLine();
 				
@@ -174,8 +109,8 @@ public class FileManagementService {
 	
 	
 	/**
-     * Metodo que comanda a inicializacao das musicas.
-     * @return Uma ArrayList com as musicas carregadas.
+     * Method that manages the songs initialization.
+     * @return An ArrayList with the loaded songs.
      */
 	public ArrayList<Music> loadMusics() {
 		ArrayList<Music> musics = new ArrayList<Music>(); 
@@ -210,8 +145,8 @@ public class FileManagementService {
 	}
 	
 	/**
-     * Metodo que comanda a inicializacao dos diretorios de musicas.
-     * @return Uma ArrayList com os diretorios de musicas que foram carregados.
+     * Method that manages the directories initialization.
+     * @return An ArrayList with the loaded directories.
      */
 	public ArrayList<String> loadDirectories() { 
 		ArrayList<String> loadedDirectories = new ArrayList<String>();
@@ -240,15 +175,37 @@ public class FileManagementService {
 	
 	
 	// Operational Methods
-	public void addMusic(String path) { /*TODO*/ }
+	public void addMusic(String path) { 
+		// TODO
+		
+	}
 	
-	public void addMusic(Music song) { /*TODO*/ }
+	public void addMusic(Music song) { 
+		// TODO
+	}
 	
 	public void addMusicToPlaylist(Playlist playlist, Music song) { /*TODO*/ }
 	
 	public void removeMusicFromPlaylist(Playlist playlist, Music song) { /*TODO*/ }
 	
-	public void addDirectory(String path) { /*TODO*/ }
+	public void addDirectory(String path) { 
+		File file = new File("/resources/data/directories.txt");
+		FileWriter fr = null;
+		
+		try {
+			fr = new FileWriter(file, true);
+			fr.write(path);
+		} catch (IOException e) {
+			// e.printStackTrace();
+		} finally {
+			try {
+				fr.close();
+			} catch (IOException e) {
+				//e.printStackTrace();
+			}
+			
+		}
+	}
 	
 	public void removeDirectory(String path) { /*TODO*/ }
 }
