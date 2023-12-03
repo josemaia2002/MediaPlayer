8b5236d1aadbf998a5ba2f3ab823d17e6d432126
Jose Maia <maianeto2014@gmail.com>
2023-12-03 10:26:01 -0300

    Documentation of the UserDao Class

--- src/br/ufrn/imd/dao/UserDao.java
+++ src/br/ufrn/imd/dao/UserDao.java
@@ -9,7 +9,23 @@ import java.util.ArrayList;
 
 import br.ufrn.imd.model.User;
 
+
+/**
+ * Data Access Object (DAO) class for managing interactions
+ * with the files that store the users.
+ * 
+ * @author Davi Matias
+ * @author Jose Maia
+ * @version 1.0
+ * @since 3/12/2023
+ */
 public class UserDao {
+	
+	/**
+	 * Method that finds all the users of the system.
+	 * 
+	 * @return An ArrayList with the found users.
+	 */
 	public ArrayList<User> findAllUsers()
 	{
 		ArrayList<User> users = new ArrayList<User>();
@@ -36,6 +52,11 @@ public class UserDao {
 		return users;
 	}
 	
+	/**
+	 * Method that finds a user of the system based on the username.
+	 * 
+	 * @return The requested user.
+	 */
 	public User findUserByUsername(String username)
 	{
 		User user = null;
@@ -50,6 +71,11 @@ public class UserDao {
 		return user;
 	}
 	
+	/**
+	 * Method that finds a user of the system based on the email.
+	 * 
+	 * @return The requested user.
+	 */
 	public User findUserByEmail(String email)
 	{
 		User user = null;
@@ -64,6 +90,15 @@ public class UserDao {
 		return user;
 	}
 	
+	/**
+	 * Method that saves a user with the given parameters.
+	 * 
+	 * @param username The username of the user.
+	 * @param userType Whether the user is vip or default.
+	 * @param email The email of the user.
+	 * @param password The password of the user.
+	 * @return True if the operation was successfull, and false otherswise.
+	 */
 	public boolean saveUser(String username, String userType, String email, String password) {
 
 		try {
@@ -84,6 +119,12 @@ public class UserDao {
 		return true;
 	}
 	
+	/**
+	 * Method that saves a given user.
+	 * 
+	 * @param user The user to be saved.
+	 * @return True if the operation was successfull, and false otherswise.
+	 */
 	public boolean saveUser(User user) {
 		return saveUser(user.getUsername(), user.getUserType(), user.getEmail(), user.getPassword());
 		
