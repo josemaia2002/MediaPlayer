bb176f8d204c81f8fdbdd3d58214e092d3f20389
Jose Maia <maianeto2014@gmail.com>
2023-12-03 13:34:36 -0300

    Documentation of the NewPlaylistController Class

--- src/br/ufrn/imd/control/NewPlaylistController.java
+++ src/br/ufrn/imd/control/NewPlaylistController.java
@@ -11,6 +11,21 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+/**
+ * The NewPlaylistController class manages the logic for creating a new playlist.
+ * It extends WindowController, providing control over the corresponding FXML window.
+ * This controller handles user input for the new playlist's name, confirmation actions,
+ * and interacts with the FileManagementService for playlist creation.
+ *
+ * The class includes methods to access the confirm button and input field.
+ * Additionally, the 'confirm' method is intended to be associated with the confirmation
+ * button's action, allowing customization of the confirmation process.
+ * 
+ * @author Davi Matias
+ * @author Jose Maia
+ * @version 1.0
+ * @since 3/12/2023
+ */
 public class NewPlaylistController extends WindowController{
 
     @FXML
@@ -18,15 +33,30 @@ public class NewPlaylistController extends WindowController{
     
     @FXML
     private Button confirmButton;
-
+    
+    /**
+     * Retrieves the confirmation button associated with creating the new playlist.
+     *
+     * @return The JavaFX Button representing the confirmation action.
+     */
 	public Button getConfirmButton() {
 		return confirmButton;
 	}
 
+	/**
+     * Retrieves the input field where the user enters the new playlist's name.
+     *
+     * @return The JavaFX TextField for entering the new playlist's name.
+     */
 	public TextField getInput() {
 		return input;
 	}
 	
+	/**
+     * Placeholder method to be associated with the confirmation button's action.
+     * Customization of this method allows for specific actions when confirming
+     * the creation of a new playlist.
+     */
 	public void confirm() {}
 
 }
